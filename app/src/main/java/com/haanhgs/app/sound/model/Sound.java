b3c0933aa887a28d2107dfe29c3f98651ee511f2
saigonjew <saigonjew@gmail.com>
2020-10-06 16:37:19 +0700

    reset

--- app/src/main/java/com/haanhgs/app/sound/model/Sound.java
+++ app/src/main/java/com/haanhgs/app/sound/model/Sound.java
@@ -2,58 +2,5 @@ package com.haanhgs.app.sound.model;
 
 public class Sound {
 
-    private int repeat = 0;
-    private float volume = 0.2f;
-    private int id1;
-    private int id2;
-    private int id3;
-    private int nowPlaying = 1;
 
-    public int getRepeat() {
-        return repeat;
-    }
-
-    public void setRepeat(int repeat) {
-        this.repeat = repeat;
-    }
-
-    public float getVolume() {
-        return volume;
-    }
-
-    public void setVolume(float volume) {
-        this.volume = volume;
-    }
-
-    public int getId1() {
-        return id1;
-    }
-
-    public void setId1(int id1) {
-        this.id1 = id1;
-    }
-
-    public int getId2() {
-        return id2;
-    }
-
-    public void setId2(int id2) {
-        this.id2 = id2;
-    }
-
-    public int getId3() {
-        return id3;
-    }
-
-    public void setId3(int id3) {
-        this.id3 = id3;
-    }
-
-    public int getNowPlaying() {
-        return nowPlaying;
-    }
-
-    public void setNowPlaying(int nowPlaying) {
-        this.nowPlaying = nowPlaying;
-    }
 }
